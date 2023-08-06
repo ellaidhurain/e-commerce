2dccafade056169e2f8f04f6cbfc4c0607629bd0
ellaidhurai n <ellaidhurai.97@gmail.com>
2023-08-06 12:05:03 +0530

    all changes completed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const itemDivs = document.querySelectorAll(".item");
+  const parentItemContainer = document.querySelectorAll(".item");
 
   // parent container
   const dropdownContainerDiv = document.createElement("div");
@@ -21,33 +21,52 @@ document.addEventListener("DOMContentLoaded", function () {
   const firstDropdownDiv = document.createElement("div");
   firstDropdownDiv.classList.add("dropdown");
 
-  const select_1 = document.createElement("select");
-  const option_size_1 = document.createElement("option");
-  option_size_1.innerText = "S";
-  select_1.appendChild(option_size_1);
+  const text1 = document.createElement("span");
+  text1.classList.add("text");
+  text1.innerText = "#1";
+  const first_size_drop_down = document.createElement("select");
+  const size_option_1 = document.createElement("option");
+  const size_option_2 = document.createElement("option");
+  size_option_1.innerText = "S";
+  size_option_2.innerText = "10";
 
-  const select_2 = document.createElement("select");
+  first_size_drop_down.append(size_option_1, size_option_2);
+
+  const first_color_drop_down = document.createElement("select");
   const option_color_1 = document.createElement("option");
+  const option_color_2 = document.createElement("option");
   option_color_1.innerText = "Colour";
-  select_2.appendChild(option_color_1);
+  option_color_2.innerText = "Red";
+  first_color_drop_down.append(option_color_1, option_color_2);
 
-  firstDropdownDiv.append(select_1, select_2);
+  firstDropdownDiv.append(text1, first_size_drop_down, first_color_drop_down);
 
   // second drop down container
   const secondDropdownDiv = document.createElement("div");
   secondDropdownDiv.classList.add("dropdown");
 
-  const select_3 = document.createElement("select");
+  const text2 = document.createElement("span");
+  text2.classList.add("text");
+  text2.innerText = "#2";
+  const second_size_drop_down = document.createElement("select");
+  const option_size_1 = document.createElement("option");
   const option_size_2 = document.createElement("option");
-  option_size_2.innerText = "S";
-  select_3.appendChild(option_size_2);
+  option_size_1.innerText = "S";
+  option_size_2.innerText = "20";
+  second_size_drop_down.append(option_size_1, option_size_2);
 
-  const select_4 = document.createElement("select");
-  const option_color_2 = document.createElement("option");
-  option_color_2.innerText = "Colour";
-  select_4.appendChild(option_color_2);
+  const second_color_drop_down = document.createElement("select");
+  const color_option_1 = document.createElement("option");
+  const color_option_2 = document.createElement("option");
+  color_option_1.innerText = "Colour";
+  color_option_2.innerText = "Blue";
+  second_color_drop_down.append(color_option_1, color_option_2);
 
-  secondDropdownDiv.append(select_3, select_4);
+  secondDropdownDiv.append(
+    text2,
+    second_size_drop_down,
+    second_color_drop_down
+  );
 
   // appending all child element to parent element
   dropdownContainerDiv.append(
@@ -57,7 +76,7 @@ document.addEventListener("DOMContentLoaded", function () {
   );
 
   function removeDropdownContainer() {
-    itemDivs.forEach((itemDiv) => {
+    parentItemContainer.forEach((itemDiv) => {
       itemDiv.classList.remove("selected");
       const dropdown = itemDiv.querySelector(".dropdown-container");
       if (dropdown) {
@@ -66,14 +85,18 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  itemDivs.forEach((itemDiv) => {
+  parentItemContainer.forEach((itemDiv) => {
     const radioInput = itemDiv.querySelector("input[type='radio']");
+
+    const totalEle = document.querySelector("#total");
+    const priceElement = itemDiv.querySelector(".price-text");
 
     radioInput.addEventListener("change", function () {
       removeDropdownContainer();
       if (radioInput.checked) {
         itemDiv.appendChild(dropdownContainerDiv);
         itemDiv.classList.add("selected");
+        totalEle.textContent = priceElement.textContent;
       }
     });
   });
